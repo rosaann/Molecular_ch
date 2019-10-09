259ced93bccf9da5721cc37b29b6d257abb16a9b
rosaann <147429240@qq.com>
2019-10-09 17:21:35 +0800

    d

--- src/utils/csv_to_pkl.py
+++ src/utils/csv_to_pkl.py
@@ -42,7 +42,7 @@ def get_datasets(root_dir, mode=""):
         with gzip.open(root_dir+'/'+f"/torch_proc_train{mode}.pkl.gz", "wb") as f:
             pickle.dump(D_train, f, protocol=4)
 
-    if os.path.exists(root_dir + full_path):
+    if 1:#os.path.exists(root_dir + full_path):
         print("Found gzipped dataset!")
         with gzip.open(root_dir + f"/torch_proc_train{mode}.pkl.gz", "rb") as f:
             train = TensorDataset(*pickle.load(f))
@@ -51,7 +51,7 @@ def get_datasets(root_dir, mode=""):
         with gzip.open(root_dir + f"/torch_proc_val{mode}.pkl.gz", "rb") as f:
             val = TensorDataset(*pickle.load(f))
         return train, val
-    else:
+  '''  else:
         with bz2.open(root_dir + f"/torch_proc_train{mode}.pkl.bz2", "rb") as f:
             train = TensorDataset(*pickle.load(f))
         if mode == "_full":
@@ -59,7 +59,7 @@ def get_datasets(root_dir, mode=""):
         with bz2.open(root_dir + f"/torch_proc_val{mode}.pkl.bz2", "rb") as f:
             val = TensorDataset(*pickle.load(f))
         return train, val
-
+'''
 def get_submission_set(root_dir, mode=""):
     full_path = "/torch_proc_submission{mode}.pkl.gz"
     if mode != "":
