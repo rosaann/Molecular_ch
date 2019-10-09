b9d58bb3db4dab9f2dc5bf2c5cc1229baaaf0977
rosaann <147429240@qq.com>
2019-10-09 17:46:03 +0800

    d

--- src/utils/csv_to_pkl.py
+++ src/utils/csv_to_pkl.py
@@ -39,8 +39,8 @@ def get_datasets(root_dir, mode=""):
         with gzip.open(root_dir + f"/torch_proc_train{mode}_p2.pkl.gz", "rb") as f:
             D_train_part2 = pickle.load(f)
         D_train = tuple([torch.cat([D_train_part1[i], D_train_part2[i]], dim=0) for i in range(len(D_train_part1))])
-        with gzip.open(root_dir+'/'+f"/torch_proc_train{mode}.pkl.gz", "wb") as f:
-            pickle.dump(D_train, f, protocol=4)
+      #  with gzip.open(root_dir+'/'+f"/torch_proc_train{mode}.pkl.gz", "wb") as f:
+     #       pickle.dump(D_train, f, protocol=4)
 
     if 1:#os.path.exists(root_dir + full_path):
         print("Found gzipped dataset!")
